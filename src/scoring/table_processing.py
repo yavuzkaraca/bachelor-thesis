82ba9449663a1e4078eaf2305628ef400e3ade78
Yavuz Karaca <yavkaraca@gmail.com>
2024-08-25 01:03:56 +0200

    Scores calculated

--- src/scoring/table_processing.py
+++ src/scoring/table_processing.py
@@ -10,12 +10,22 @@ def calculate_metrics(tp, fp, fn):
 
 
 # Read the CSV file
-input_file = '../../dataset/PSE/2021_init-v/exploration.csv'  # Replace with your input file name
-output_file = '../../dataset/PSE/2021_init-v/exploration.csv'  # Replace with your desired output file name
+file_path = '../../out/advanced/advanced_score.csv'  # Replace with your input file name
 
-df = pd.read_csv(input_file)
+df = pd.read_csv(file_path)
 
-# Iterate through the DataFrame and calculate the metrics
+"""
+for index, row in df.iterrows():
+    tp1 = row['L1 tp']
+    tp2 = row['L2 tp']
+    tp3 = row['L3 tp']
+
+    total_tp = tp1 + tp2 + tp3
+
+    df.at[index, 'Total tp'] = total_tp
+"""
+
+
 for index, row in df.iterrows():
     tp = row['Total tp']
     fp = row['Total fp']
@@ -23,9 +33,10 @@ for index, row in df.iterrows():
 
     precision, recall, f1_score = calculate_metrics(tp, fp, fn)
 
-    df.at[index, 'Recall'] = recall
-    df.at[index, 'Precision'] = precision
-    df.at[index, 'F1 score'] = f1_score
+    df.at[index, 'Total Recall'] = recall
+    df.at[index, 'Total Precision'] = precision
+    df.at[index, 'Total F1 score'] = f1_score
+
 
 # Save the updated DataFrame back to a CSV file
-df.to_csv(output_file, index=False)
+df.to_csv(file_path, index=False)
